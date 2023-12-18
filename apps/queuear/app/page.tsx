f4fd912479d22a5768f8e4bd84c6febee2eb71fb
Albert Ferguson <albertferguson118@gmail.com>
2023-12-18 21:14:19 +1100

    use new datalib + hooks

--- apps/queuear/app/page.tsx
+++ apps/queuear/app/page.tsx
@@ -1,12 +1,16 @@
 import HomePage, { PropType } from './home-page';
+import {
+  useCurrentDeployment,
+  usePlannedDeployments,
+  useEnvironmentInfo,
+} from '@queuear/data';
 
 export async function getData() {
   const deploymentRes = await fetch('http://localhost:4200/api/deployments');
+
   const currentDeploymentInfo = await deploymentRes.json();
 
-  const groupedDeploymentsRes = await fetch(
-    'http://localhost:4200/api/grouped-deployments'
-  );
+  const groupedDeploymentsRes = await fetch('http://localhost:4200/api/');
   const groupedDeployments = await groupedDeploymentsRes.json();
 
   const environmentRes = await fetch('http://localhost:4200/api/environment');
@@ -20,6 +24,15 @@ export async function getData() {
 }
 
 export default async function Page() {
-  const data = await getData();
+  const { currentDeployment } = useCurrentDeployment();
+  const { plannedDeployments } = usePlannedDeployments();
+  const { environmentInfo } = useEnvironmentInfo();
+
+  const data = {
+    currentDeploymentInfo: currentDeployment,
+    groupedDeployments: plannedDeployments,
+    environment: environmentInfo,
+  } as PropType;
+
   return <HomePage props={data} />;
 }
