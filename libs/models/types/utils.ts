95e689897c5f8f1b4057eaf5616b5bf233eda749
Albert Ferguson <albertferguson118@gmail.com>
2023-12-18 21:35:49 +1100

    disable eslint on file

--- libs/models/types/utils.ts
+++ libs/models/types/utils.ts
@@ -1,5 +1,6 @@
 // Return an enum as an iterable of the string keys
 // for convenience this method defines data as 'any' but it should be a ts enum
-export function enumAsArray(data: any): String[] {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function enumAsArray(data: any): string[] {
   return Object.keys(data).filter((v) => isNaN(Number(v)));
 }
